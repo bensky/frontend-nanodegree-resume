0a877c2c01ce5e0238eb6776fcd36e3e584ba6bf
bensky <ben@krokosky.com>
2015-02-18 12:03:03 -0500

    Changing over to use object lieteral notaion, per the course

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,20 @@
-var name="Benjamin H. Krokosky"
-var role="Software Manager"
+var bio = {
+	"name" : "Benjamin H. Krokosky",
+	"role" : "Software Manager",
+	"contact_info" : "118 Pauline Drive, Monroeville PA, 15146",
+	"pic_URL" : "https://yt3.ggpht.com/-4stmkIg4g9I/AAAAAAAAAAI/AAAAAAAAAAA/KQHXanuFIXo/s100-c-k-no/photo.jpg",
+	"welcome_message" : "Welcome to my interactive resume",
+	"skills" : skills
+};
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contact_info);
 
+$
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#topContacts").prepend(formattedContact);
+
+var skills = ["C","C++", "HTML", "JS", "Management"];
+
