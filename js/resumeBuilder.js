c0cf8989f1179912f0b789dc591c940744dae604
unknown <bkrokosky@x2136.compunet.local>
2015-02-17 20:58:12 -0500

    Added changes to display my name and title

---
+++ js/resumeBuilder.js
@@ -0,0 +1,8 @@
+var name="Benjamin H. Krokosky"
+var role="Software Manager"
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
